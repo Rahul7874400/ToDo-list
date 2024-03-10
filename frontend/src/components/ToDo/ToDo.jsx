12eabb542a2d0e5f5506f1a08c320471bf8cedbd
Rahul <rkkraj500@gmail.com>
2024-03-10 22:45:43 +0530

    Todo completed

--- frontend/src/components/ToDo/ToDo.jsx
+++ frontend/src/components/ToDo/ToDo.jsx
@@ -1,15 +1,54 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
+import axios from "axios"
 
-const TodoForm = ({ addTodo, hideTodoList, showTodoList }) => {
+const TodoForm =  ({ addTodo, hideTodoList, showTodoList }) => {
   const [todo, setTodo] = useState('');
+  const[todoList , setToDoList] = useState([])
 
-  const handleSubmit = (e) => {
+  const handleSubmit = async(e) => {
     e.preventDefault();
-    if (todo.trim() !== '') {
-      addTodo(todo);
-      setTodo('');
-    }
+    const userId = localStorage.getItem('userId')
+    await axios.get('/api/v1/todo/create',{
+     todo,
+     params : {
+      userId
+     }
+    }).then( (res)=>{
+      console.log(res)
+    } ).catch( (error)=>{
+      console.log("something went worng while adding todo : ",error)
+    })
   };
+
+  const showTodoList = async(e) =>{
+    const userId = localStorage.getItem('userId')
+    await axios.get('/api/v1/todo//create/getList')
+    .then( (res)=>{
+      setToDoList(res)
+      const div = document.createElement('div')
+      for(let i=0;i<res.length;i++){
+        <div id= {res.data.data[i]._id}> { res.data.data[i].toto } </div>
+      }
+      console.log(res)
+    } )
+    .catch( (error)=>{
+      console.log("Something went worng while fetching Todo",error)
+    } )
+  }
+
+  useEffect( async(e)=>{
+    const userId = localStorage.getItem('userId')
+    await axios.get('/api/v1/todo//create/getList')
+    .then( (res)=>{
+      setToDoList(res.data.data)
+      const div = document.createElement('div')
+      
+      console.log(res)
+    } )
+    .catch( (error)=>{
+      console.log("Something went worng while fetching Todo",error)
+    } )
+  },[] )
 
   return (
     <div>
@@ -25,14 +64,17 @@ const TodoForm = ({ addTodo, hideTodoList, showTodoList }) => {
           <button type="submit" className="px-4 py-2 bg-blue-500 text-white rounded-md">
             Add
           </button>
+
+          {
+            todo.map( (todo , index)=>{
+              <div id= {todo._id}>
+                <h3>todo.todo</h3>
+              </div>
+            } )
+          }
         </div>
       </form>
-      <button onClick={showTodoList} className="px-4 py-2 bg-green-500 text-white rounded-md mr-2">
-        Show Todos
-      </button>
-      <button onClick={hideTodoList} className="px-4 py-2 bg-red-500 text-white rounded-md">
-        Hide Todos
-      </button>
+      
     </div>
   );
 };
